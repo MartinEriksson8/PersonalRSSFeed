4da874cd693ca0158812c11e727c43a6786bf99f
Martin-Desktop <martineriksson8@gmail.com>
2019-01-23 14:27:19 +0100

    initial commit

---
+++ app/src/main/java/org/jeltz/vogon/personalrssfeed/RssFeed.java
@@ -0,0 +1,20 @@
+package org.jeltz.vogon.personalrssfeed;
+
+public class RssFeed {
+
+    public String rssFeedTitle;
+    public String rssFeedAdress;
+    public int id;
+
+    public RssFeed(String title, String adress){
+        rssFeedTitle title;
+        rssFeedAdress adress;
+    }
+
+    public RssFeed(int feedId, String title, String adress){
+        rssFeedTitle title;
+        rssFeedAdress adress;
+        id = feedId;
+    }
+
+}
